a4cf5488d4267a015da04c894e3ddb3a660f6bb4
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-04 16:33:43 +0000

    Item initialization test

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,21 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
-  });
+  var item;
+  var shop;
 
+  describe('Item', function(){
+
+    beforeEach(function(){
+      item = new Item('name', 0, 0);
+    });
+
+    describe('#constructor', function(){
+      it('should set the parameters name, sellIn and quality upon initalization', function() {
+        expect(item.name).toEqual('name');
+        expect(item.sellIn).toEqual(0);
+        expect(item.quality).toEqual(0);
+      });
+    });
+
+  });
 });
