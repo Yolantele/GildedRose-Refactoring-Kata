f43b4bef3b80079f4bd226babe2984004fba0959
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-02-05 15:06:09 +0000

    DRYs out constants

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -7,17 +7,22 @@ class Item {
   }
 }
 
+const backPass = 'Backstage passes to a TAFKAL80ETC concert';
+const sulfuras = 'Sulfuras, Hand of Ragnaros';
+const brie = 'Aged Brie';
+
 class Shop {
   constructor(items = []) {
     this.items = items;
   }
+
   // find all the aged brie function
 
   isRegular(name) {
     switch (name) {
-      case 'Backstage passes to a TAFKAL80ETC concert':
-      case 'Aged Brie':
-      case 'Sulfuras, Hand of Ragnaros':
+      case backPass:
+      case brie:
+      case sulfuras:
         return false;
         break;
       default:
@@ -27,12 +32,8 @@ class Shop {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       const item = this.items[i];
-      const backPass = 'Backstage passes to a TAFKAL80ETC concert';
-      const sulfuras = 'Sulfuras, Hand of Ragnaros';
-      const brie = 'Aged Brie';
 
       if (item.quality < 50 && item.quality > 0) {
-      // regurlar item decrease decrease Q when :
         if (item.name !== brie && item.name !== backPass) {
             if (item.name !== sulfuras) {
               item.quality -= 1;
@@ -41,14 +42,12 @@ class Shop {
           item.quality += 1;
           if (item.name === backPass) {
             if (item.sellIn < 11) {
-              if (item.quality < 50) {
+              if (item.quality < 50 && item.quality > 0) {
                 item.quality += 1;
               }
             }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
+            if (item.sellIn < 6 && item.quality < 50) {
                 item.quality += 1;
-              }
             }
           }
         }
@@ -56,13 +55,12 @@ class Shop {
           item.sellIn -= 1;
         }
         if (item.sellIn < 0) {
+          // if(this.isRegular(item.name) ){
           if (item.name !== brie ) {
             if (item.name !== backPass) {
-              if (item.quality > 0) {
                 if (item.name !== sulfuras) {
                   item.quality -= 1;
                 }
-              }
             } else {
               item.quality = 0;
             }
