d52ad1a0f5a3c902a7a010ad8fbbaf7b95e4c6f4
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-02-06 10:52:55 +0000

    Extracts QUality range check

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -11,11 +11,13 @@ const backPass = 'Backstage passes to a TAFKAL80ETC concert';
 const sulfuras = 'Sulfuras, Hand of Ragnaros';
 const brie = 'Aged Brie';
 
+const MAX = 50;
+const MIN = 0;
+
 class Shop {
   constructor(items = []) {
     this.items = items;
   }
-
   isRegular(name) {
     switch (name) {
       case backPass:
@@ -27,11 +29,15 @@ class Shop {
       return true;
     }
   }
+  isWithinQualityRange(item){
+    if (item.quality < MAX && item.quality > MIN) {
+      return true;
+    }
+  }
   updateRegular(item){
     item.quality -= 1;
     item.sellIn -= 1;
   }
-
   updateBackPass(item) {
     item.sellIn -= 1;
     if (item.sellIn > 11) {item.quality += 1;}
@@ -39,19 +45,21 @@ class Shop {
     if (item.sellIn < 6) {item.quality += 3;}
     if (item.sellIn < 0) {item.quality = 0;}
   }
-
+  updateBrie(item){
+    item.quality += 1;
+  }
   updatePostExpiry(item) {
-    if (item.name === brie) {item.quality += 1;}
     if (item.name !== sulfuras) {item.quality = 0;}
   }
 
   updateQuality() {
-    var that = this;
+    var then = this;
     this.items.forEach(item => {
-      if (item.quality < 50 && item.quality > 0) {
-        if (that.isRegular(item.name)) { that.updateRegular(item);}
-        if (item.name === backPass) { that.updateBackPass(item);}
-        if (item.sellIn < 0) { that.updatePostExpiry(item);}
+      if (then.isWithinQualityRange(item)) {
+        if (then.isRegular(item.name)) {then.updateRegular(item);}
+        if (item.name === backPass) {then.updateBackPass(item);}
+        if (item.name === brie) {then.updateBrie(item);}
+        if (item.sellIn < 0) {then.updatePostExpiry(item);}
       }
     })
     return this.items;
