a3fa93ffb224ba3b3254e1255335a6c5ba8c95f2
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-02-06 10:35:25 +0000

    Extracts update Post Expiry Items

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -34,34 +34,24 @@ class Shop {
 
   updateBackPass(item) {
     item.sellIn -= 1;
-    if (item.sellIn > 11) {
-      item.quality += 1;
-    } if (item.sellIn < 11 && item.sellIn >= 6 ) {
-      item.quality += 2;
-    } if (item.sellIn < 6) {
-      item.quality += 3;
-    } if (item.sellIn < 0) {
-      item.quality = 0;
-    }
+    if (item.sellIn > 11) {item.quality += 1;}
+    if (item.sellIn < 11 && item.sellIn >= 6 ) {item.quality += 2;}
+    if (item.sellIn < 6) {item.quality += 3;}
+    if (item.sellIn < 0) {item.quality = 0;}
+  }
+
+  updatePostExpiry(item) {
+    if (item.name === brie) {item.quality += 1;}
+    if (item.name !== sulfuras) {item.quality = 0;}
   }
 
   updateQuality() {
     var that = this;
     this.items.forEach(item => {
       if (item.quality < 50 && item.quality > 0) {
-        if (that.isRegular(item.name)) {
-          that.updateRegular(item);
-        }
-        if (item.name === backPass) {
-          that.updateBackPass(item);
-        }
-        if (item.sellIn < 0) {
-          if (item.name === brie) {
-            item.quality += 1;
-          } if (item.name !== sulfuras) {
-            item.quality = 0;
-          }
-        }
+        if (that.isRegular(item.name)) { that.updateRegular(item);}
+        if (item.name === backPass) { that.updateBackPass(item);}
+        if (item.sellIn < 0) { that.updatePostExpiry(item);}
       }
     })
     return this.items;
